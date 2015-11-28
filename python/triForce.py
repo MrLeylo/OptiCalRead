5e10a2f4638d62b4cb84aa7ca87d091c0b2a0c14
MrLeylo <i.masmend@gmail.com>
2015-11-28 10:58:43 +0100

    Deleted useless files

--- python/triForce.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-import os
-while True:
-	os.fork()
--- python/try5.py
+++
@@ -1,6 +0,0 @@
-import ownSegGenerator as sg
-import fulldataBase as dB
-
-dB.readCROHMEdB(['trainData/CROHME_training','trainData/trainData_v2','trainData/TrainINKML'])
-
-
